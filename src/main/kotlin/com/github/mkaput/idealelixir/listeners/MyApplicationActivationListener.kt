032b0b78f3e3b6e7737377331829a33ae2085f62
GitHub Action <action@github.com>
2024-03-17 17:14:41 +0000

    template cleanup

--- src/main/kotlin/com/github/mkaput/idealelixir/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/mkaput/idealelixir/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mkaput.idealelixir.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
